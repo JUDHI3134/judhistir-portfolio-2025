1c0d48d9f160bfe3288c9cdb4cad5cb2b41d1c3b
JUDHI3134 <judhistirbehera241@gmail.com>
2025-01-05 10:40:00 +0530

    create my project section

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
+import Project from "@/components/Project";
 import Services from "@/components/Services";
 
 
@@ -12,6 +13,7 @@ export default function Home() {
    <Header />
    <About />
    <Services />
+   <Project />
    </>
   );
 }
