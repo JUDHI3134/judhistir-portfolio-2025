da8951164ebd3041d86a9717498aa87171b54913
JUDHI3134 <judhistirbehera241@gmail.com>
2025-01-05 12:07:12 +0530

    create footer

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Project from "@/components/Project";
@@ -16,6 +17,7 @@ export default function Home() {
    <Services />
    <Project />
    <Contact />
+   <Footer />
    </>
   );
 }
