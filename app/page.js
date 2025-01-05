0e5b219e19c3b1ab349ec6665c2736986d92553f
JUDHI3134 <judhistirbehera241@gmail.com>
2025-01-05 11:51:44 +0530

    create contact form

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client"
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Project from "@/components/Project";
@@ -14,6 +15,7 @@ export default function Home() {
    <About />
    <Services />
    <Project />
+   <Contact />
    </>
   );
 }
